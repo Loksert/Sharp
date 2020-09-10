e2f618fbff460dbd1efce08cb62183e868a02ed1
Damond LOKS <super.dimasik53@yandex.ru>
2020-09-10 21:14:35 +0300

    Commit

---
+++ C_Sharp_Learn/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace C_Sharp_Learn
+{
+
+
+    class Program
+    {
+        public static bool sumTwoSmallestNumbers(string f)
+        {
+            string result = new string(f.Where(t => char.IsDigit(t)).ToArray());
+            if (result == f)
+            {
+                if (f.Length == 6 || f.Length == 4)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            var a = sumTwoSmallestNumbers("345566");
+            Console.WriteLine(a);
+        }
+    }
+}
